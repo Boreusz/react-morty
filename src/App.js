e5aa50dff1b7ffe20160488b055e39f26c000473
Mateusz <matt.borowczyk@gmail.com>
2021-04-19 16:07:23 +0200

    add states, essential props to Links, formating

--- src/App.js
+++ src/App.js
@@ -3,55 +3,85 @@ import {
   Switch,
   Route,
   Link,
-  NavLink
-} from "react-router-dom";
+  NavLink,
+} from 'react-router-dom';
+import { useState } from 'react';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-import {ReactComponent as ReactLogo} from './assets/Rick_and_Morty.svg';
-import Home from './components/Home'
-import Favorites from './components/Favorites'
+import { ReactComponent as ReactLogo } from './assets/Rick_and_Morty.svg';
+import Home from './components/Home';
+import Favorites from './components/Favorites';
 import Search from './components/Search';
 import './styles/App.scss';
 
 const client = new ApolloClient({
   uri: 'https://rickandmortyapi.com/graphql',
-  cache: new InMemoryCache()
-})
+  cache: new InMemoryCache(),
+});
 
 const App = () => {
+  let [searchValue, setSearchValue] = useState('');
+  let [activePage, setActivePage] = useState('1');
+  let [favoritesList, setFavoritesList] = useState([]);
+  const setSearch = (value) => {
+    setSearchValue((searchValue = `name: "${value}"`));
+  };
   return (
     <ApolloProvider client={client}>
-      <div className="App">
+      <div className='App'>
         <Router>
-          <div className="header">
-            <Link to="/"><ReactLogo className="header__logo"/></Link>
-            <Search />
+          <div className='header'>
+            <Link to='/'>
+              <ReactLogo className='header__logo' />
+            </Link>
+            <Search setSearchValue={setSearch} />
           </div>
           <hr />
-          <div>
-            <nav className="nav">
-              <ul className="nav__list">
-                <li className="nav__element">
-                  <NavLink exact className="nav__link" activeClassName="nav__link--active" to="/">All Characters</NavLink>
+          <main>
+            <nav className='nav'>
+              <ul className='nav__list'>
+                <li className='nav__element'>
+                  <NavLink
+                    exact
+                    className='nav__link'
+                    activeClassName='nav__link--active'
+                    to='/'
+                  >
+                    All Characters
+                  </NavLink>
                 </li>
-                <li className="nav__element">
-                  <NavLink className="nav__link" activeClassName="nav__link--active" to="/favorites">Favorites</NavLink>
+                <li className='nav__element'>
+                  <NavLink
+                    className='nav__link'
+                    activeClassName='nav__link--active'
+                    to='/favorites'
+                  >
+                    Favorites
+                  </NavLink>
                 </li>
               </ul>
             </nav>
-            
+
             <Switch>
-              <Route exact path="/">
-                <Home />
+              <Route exact path='/'>
+                <Home
+                  searchInput={searchValue}
+                  page={activePage}
+                  favorites={favoritesList}
+                  setFavorites={setFavoritesList}
+                />
               </Route>
-              <Route path="/Favorites">
-                <Favorites />
+              <Route path='/Favorites'>
+                <Favorites
+                  favorites={favoritesList}
+                  setFavorites={setFavoritesList}
+                />
               </Route>
             </Switch>
-          </div>
+          </main>
         </Router>
       </div>
     </ApolloProvider>
   );
-}
+};
 
 export default App;
