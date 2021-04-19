940680eb6cab1dc4a9eaf524ea34b412966c010e
Mateusz <matt.borowczyk@gmail.com>
2021-04-19 16:05:53 +0200

    formating & deleting useless component

--- src/components/CustomQuery.js
+++ src/components/CustomQuery.js
@@ -1,16 +1,21 @@
-import React from 'react'
+import React from 'react';
 import { useQuery } from '@apollo/client';
 import { query } from '../GraphQL/Queries';
 import '../styles/Query.scss';
 import QueryComponent from './QueryComponent';
+import Loading from './Loading';
 
-const CustomQuery = ({page, filters}) => {
-  const { error, loading, data } = useQuery(query());
-  if (loading) return 'Loading...';
-  if(error) return {error}
+const CustomQuery = ({ page, filters, favorites, setFavorites }) => {
+  const { error, loading, data } = useQuery(query(page, filters));
+  if (loading) return <Loading />;
+  if (error) return error.message;
   return (
-    <QueryComponent data={data}/>
+    <QueryComponent
+      data={data}
+      favorites={favorites}
+      setFavorites={setFavorites}
+    />
   );
-}
+};
 
-export default CustomQuery
+export default CustomQuery;
--- src/components/Gender.js
+++ src/components/Gender.js
@@ -1,30 +1,30 @@
 import React from 'react';
-import { BiMaleSign, BiFemaleSign } from 'react-icons/bi'
-import { FaGenderless } from 'react-icons/fa'
-import { BsDash } from 'react-icons/bs'
+import { BiMaleSign, BiFemaleSign } from 'react-icons/bi';
+import { FaGenderless } from 'react-icons/fa';
+import { BsDash } from 'react-icons/bs';
 
-const Gender = ({gender}) => {
+const Gender = ({ gender }) => {
   let element;
   switch (gender) {
     case 'Male':
-      element = <BiMaleSign className='character__icon'/>;
+      element = <BiMaleSign className='character__icon' />;
       break;
-    case "Female":
-      element = <BiFemaleSign className='character__icon'/>
+    case 'Female':
+      element = <BiFemaleSign className='character__icon' />;
       break;
-    case "Genderless":
-      element = <FaGenderless className='character__icon'/>
+    case 'Genderless':
+      element = <FaGenderless className='character__icon' />;
       break;
     default:
-      element = <BsDash className='character__icon'/>
+      element = <BsDash className='character__icon' />;
   }
 
-  return(
+  return (
     <>
       {element}
       <span className='character__gender'>{gender}</span>
     </>
-  )
-}
+  );
+};
 
-export default Gender
+export default Gender;
