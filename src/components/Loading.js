f5cc90c0abec5f687980db8d6f92171e7f0bace3
Mateusz <matt.borowczyk@gmail.com>
2021-04-19 16:06:29 +0200

    add search logic and needed components

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import ReactLoading from 'react-loading';
+import '../styles/App.scss'
 
 const Loading = () => {
   return (
-    <div>
-      
-    </div>
+    <section className="loading">
+      <h2>Your Query is being processed</h2>
+      <ReactLoading className="loading" type={'spinningBubbles'} color={'#11B0C8'} height={'10%'} width={'10%'} />
+    </section>
   )
 }
 
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import debounce from 'lodash.debounce'
 import { BsSearch } from 'react-icons/bs';
 import '../styles/Search.scss';
 
-const Search = () => {
+const Search = ({ setSearchValue }) => {
+  
+  const debounced = useCallback(
+    debounce((e) => {
+      setSearchValue(e.target.value)
+      // send the server request here		
+    }, 1000),
+    []
+  );
+
   return (
     <div className="search-wrapper">
-      <input placeholder="Start typing to search..."></input>
+      <input placeholder="Start typing name to search..." onChange={ (e) => debounced(e)}></input>
       <BsSearch className='search-icon'/>
     </div>
   )
